63f620741d9dfa31d3e7e9590723d00ece65c695
Collection50 <fkaus65@naver.com>
2023-11-11 22:25:49 +0900

    feat(MenUValidator): 음료만  입력하는 경우 오류를 발생하는 validateMenuTypes  메서드 구현

--- src/Model/Validator/MenuValidator/index.js
+++ src/Model/Validator/MenuValidator/index.js
@@ -1,7 +1,7 @@
 import Validator from '../index.js';
 import Parser from '../../Parser/index.js';
 import ValidationError from '../ValidationError/index.js';
-import { ERROR, MENUS, MENU } from '../../../constants/index.js';
+import { ERROR, BEVERAGE, MENUS, MENU } from '../../../constants/index.js';
 
 class MenuValidator extends Validator {
   static validateDuplication(menus) {
@@ -36,6 +36,15 @@ class MenuValidator extends Validator {
     });
   }
 
+  static validateMenuTypes(menus) {
+    const menuNames = menus.map(({ menu }) => menu);
+    const allConsistOfDesserts = menuNames.every((menu) => BEVERAGE[menu]);
+
+    if (allConsistOfDesserts) {
+      throw new ValidationError(ERROR.invalidMenu);
+    }
+  }
+
   static validateMenus(answer) {
     const menus = Parser.splitMenu(answer);
 
@@ -43,6 +52,7 @@ class MenuValidator extends Validator {
     this.validateMaxCount(menus);
     this.validatePurchaseCount(menus);
     this.validateIncludesMenu(menus);
+    this.validateMenuTypes(menus);
   }
 }
 
