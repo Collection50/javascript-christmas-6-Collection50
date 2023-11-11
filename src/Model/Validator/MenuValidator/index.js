ed0ba34babf8573d3a1a4537d55a37c81a3174ca
Collection50 <fkaus65@naver.com>
2023-11-11 22:21:26 +0900

    feat(MenUValidator): 20개 이상의 메뉴를 입력한 경우 오류를 발생하는 validateMaxCount 메서드 구현

--- src/Model/Validator/MenuValidator/index.js
+++ src/Model/Validator/MenuValidator/index.js
@@ -1,7 +1,7 @@
 import Validator from '../index.js';
 import Parser from '../../Parser/index.js';
 import ValidationError from '../ValidationError/index.js';
-import { ERROR } from '../../../constants/index.js';
+import { ERROR, MENU } from '../../../constants/index.js';
 
 class MenuValidator extends Validator {
   static validateDuplication(menus) {
@@ -11,10 +11,18 @@ class MenuValidator extends Validator {
     }
   }
 
+  static validateMaxCount(menus) {
+    const menuCount = menus.reduce((acc, { count }) => acc + count, 0);
+    if (menuCount > MENU.maxCount) {
+      throw new ValidationError(ERROR.invalidMenu);
+    }
+  }
+
   static validateMenus(answer) {
     const menus = Parser.splitMenu(answer);
 
     this.validateDuplication(menus);
+    this.validateMaxCount(menus);
   }
 }
 
