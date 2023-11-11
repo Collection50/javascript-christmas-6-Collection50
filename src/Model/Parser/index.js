7bc30dba68272cdcf03b940d377024682b8cf954
Collection50 <fkaus65@naver.com>
2023-11-11 22:48:14 +0900

    feat(Parser): 입력받은 메뉴를 파싱하는 parseMenu, splitMenu  메서드 구현

--- src/Model/Parser/index.js
+++ src/Model/Parser/index.js
@@ -1,6 +1,19 @@
+import { SYMBOLS } from '../../constants/index.js';
+
 class Parser {
   static parseNumber(input) {
     return Number(input);
+  }
+
+  static splitMenu(input) {
+    return input.split(SYMBOLS.menuDivider);
+  }
+
+  static parseMenu(input) {
+    return this.splitMenu(input).map((menus) => {
+      const [menu, count] = menus.split(SYMBOLS.hyphen);
+      return { menu, count: Number(count) };
+    });
   }
 }
 
