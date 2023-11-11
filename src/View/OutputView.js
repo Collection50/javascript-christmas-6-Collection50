26bdfa5f56e160e2a18ba79c540f9de453e7a2f6
Collection50 <fkaus65@naver.com>
2023-11-11 22:51:07 +0900

    feat(InputView): 안녕하세요! 우테코 식당 12월 이벤트 플래너입니다. 출력 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,8 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
+import { PLANNER_MESSAGE } from '../constants/index.js';
 
 const OutputView = {
   log(string) {
     Console.print(string);
+  },
+
+  welcome() {
+    Console.print(PLANNER_MESSAGE.welcome);
   },
 };
 
