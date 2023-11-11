534ff589df96c9b73c12e61c0199564279761b97
Collection50 <fkaus65@naver.com>
2023-11-11 22:19:54 +0900

    feat(MenUValidator): 중복 메뉴를 입력한 경우 오류를 발생하는 validateDuplication 메서드 구현

---
+++ src/Model/Validator/MenuValidator/index.js
@@ -0,0 +1,21 @@
+import Validator from '../index.js';
+import Parser from '../../Parser/index.js';
+import ValidationError from '../ValidationError/index.js';
+import { ERROR } from '../../../constants/index.js';
+
+class MenuValidator extends Validator {
+  static validateDuplication(menus) {
+    const menuNames = menus.map(({ menu }) => menu);
+    if (menuNames.length !== new Set(menuNames).size) {
+      throw new ValidationError(ERROR.invalidMenu);
+    }
+  }
+
+  static validateMenus(answer) {
+    const menus = Parser.splitMenu(answer);
+
+    this.validateDuplication(menus);
+  }
+}
+
+export default MenuValidator;
