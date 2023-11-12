04e1da1d8aceaad70b9a765f5721645d1a789c46
Collection50 <fkaus65@naver.com>
2023-11-12 23:16:42 +0900

    feat(Day): 주중/주말 여부를 문자열로 반환하는 parseDayType 메서드 구현

--- src/Model/Day/index.js
+++ src/Model/Day/index.js
@@ -13,6 +13,14 @@ class Day {
   isSpecialDay() {
     return this.#day === DATE.christmas || this.#date.getDay() === DATE.sunday;
   }
+
+  parseDayType() {
+    const day = this.#date.getDay();
+    if (day === DATE.friday || day === DATE.saturday) {
+      return DATE.weekend;
+    }
+    return DATE.weekday;
+  }
 }
 
 export default Day;
