ea42a5a590b885563f8a4ec1d577ce5754f03ef7
Collection50 <fkaus65@naver.com>
2023-11-12 23:02:20 +0900

    feat(Order): 입력받은 메뉴를 토대로 메뉴 객체 배열을 생성하는 addMenus 메서드 구현

---
+++ src/Model/Order/index.js
@@ -0,0 +1,32 @@
+import Parser from '../Parser/index.js';
+import Main from '../Menu/Main/index.js';
+import Appetizer from '../Menu/Appetizer/index.js';
+import Dessert from '../Menu/Dessert/index.js';
+import Beverage from '../Menu/Beverage/index.js';
+import { SYMBOLS } from '../../constants/index.js';
+
+const MENU_INSTANCES = {
+  main: (name, count) => new Main(name, count),
+  appetizer: (name, count) => new Appetizer(name, count),
+  dessert: (name, count) => new Dessert(name, count),
+  beverage: (name, count) => new Beverage(name, count),
+};
+
+class Order {
+  #menus;
+
+  constructor(menus) {
+    this.#menus = this.addMenus(menus);
+  }
+
+  addMenus(menus) {
+    return menus.split(SYMBOLS.menuDivider).map((menu) => {
+      const [name, count] = menu.split(SYMBOLS.hyphen);
+      const type = Parser.parseMenuType(name);
+      const menuInstance = MENU_INSTANCES[type];
+      return menuInstance(name, count);
+    });
+  }
+}
+
+export default Order;
