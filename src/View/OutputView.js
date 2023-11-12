a212d3719c1df54a4fdc739cf7190ae59c7a559c
Collection50 <fkaus65@naver.com>
2023-11-12 22:38:38 +0900

    feat(OutputView): 12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기출력하는 preview 메서드 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -9,6 +9,10 @@ const OutputView = {
   welcome() {
     Console.print(PLANNER_MESSAGE.welcome);
   },
+
+  preview(day) {
+    Console.print(`12월 ${day}일에 ${PLANNER_MESSAGE.preview}`);
+  },
 };
 
 export default OutputView;
